0766f087054364ffc028633ac736dd0a3da17099
Melvyn Guy <melvyn.guy@etu.univ-nantes.fr>
2024-02-03 14:55:14 +0100

    ajout d'une led pour avoir un retour

--- Prototype/ir_esp32.ino
+++ Prototype/ir_esp32.ino
@@ -6,6 +6,7 @@
 
 #define IR_PIN 4
 int RECV_PIN = 2;
+int ledPin = 12;
 
 IRsend irsend(IR_PIN);  // Set the GPIO to be used to sending the message.
 IRrecv irrecv(RECV_PIN);
@@ -18,14 +19,17 @@ void setup() {
   Serial.println("Enabling IRin");
   irrecv.enableIRIn(); // Start the receiver
   Serial.println("Enabled IRin");
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   if (irrecv.decode(&results)) {
     Serial.println(results.value, HEX);
     irrecv.resume(); // Receive the next value
+    digitalWrite(ledPin, HIGH);
   }
   delay(100);
+  digitalWrite(ledPin, LOW);
   Serial.println("NEC");
   irsend.sendNEC(0x00FFE01FUL);
   delay(1000);
